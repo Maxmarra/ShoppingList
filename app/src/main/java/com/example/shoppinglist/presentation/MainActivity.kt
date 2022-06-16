c7bf43777dae9db64d82c3639a78dc56f821946e
Maxmarra <5919669@gmail.com>
2022-06-16 18:47:25 +0300

    2

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -8,28 +8,4 @@ import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
-
-    //проверка удаления элемента
-    private var count = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this){
-            //проверка создания списка
-            Log.d("MainActivityTest", it.toString())
-            //проверка удаления элемента
-            //проверка редактирования (не делать вместе!!!)
-            if(count == 0) {
-                count++
-                val item = it[0]
-                //viewModel.deleteShopItem(item)
-                viewModel.changeEnableState(item)
-            }
-        }
-
-    }
 }
