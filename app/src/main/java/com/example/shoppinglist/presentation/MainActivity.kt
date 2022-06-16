38f486de51623093d18ddc127f89d748784a3b84
Maxmarra <5919669@gmail.com>
2022-06-16 18:48:35 +0300

    2

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -7,5 +7,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
